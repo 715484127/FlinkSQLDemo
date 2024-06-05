59d31931671f43e9a435112d9e0d18099595731e
wangyuepeng <715484127@qq.com>
2024-06-05 23:24:38 +0800

    dd

---
+++ src/main/java/com/my/flink/FlinkSqlReadKafkaExample.java
@@ -0,0 +1,35 @@
+package com.my.flink;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+public class FlinkSqlReadKafkaExample {
+    public static void main(String[] args) throws Exception {
+        // 设置执行环境
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1); // 根据实际情况设置并行度
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+
+        // 创建一个表来表示Kafka源，这里假设消息体为JSON格式且包含字段"text"
+        String kafkaSourceDDL = "CREATE TABLE kafkaSource (\n" +
+                "  text STRING\n" +
+                ") WITH (\n" +
+                "  'connector' = 'kafka',\n" +
+                "  'topic' = 'flink-sql-test',\n" +
+                "  'properties.bootstrap.servers' = '192.168.50.185:9093,192.168.50.185:9094,192.168.50.185:9095',\n" +
+                "  'format' = 'json',\n" +
+                "  'properties.group.id' = 'myGroup'\n" +
+                ")";
+
+        // 执行DDL创建表
+        tEnv.executeSql(kafkaSourceDDL);
+
+        // 查询Kafka数据并打印到控制台
+        String query = "SELECT text FROM kafkaSource";
+        tEnv.toRetractStream(tEnv.sqlQuery(query), String.class)
+                .print();
+
+        // 启动Flink作业
+        env.execute("Flink SQL Read from Kafka");
+    }
+}
